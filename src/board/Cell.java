365a3c221ecc6a54d91bffc62ee2e2efeedc4a2d
HumanBooster <bclerc@humanbooster.com>
2014-12-02 10:56:49 +0100

    refactor Unit (ajout list)

--- src/board/Cell.java
+++ src/board/Cell.java
@@ -1,23 +1,38 @@
 package board;
 
 import java.awt.Point;
+import java.util.List;
 
 import unit.Unit;
 
 public abstract class Cell {
 	
 	protected Point point;
+	
+	Cell() {
+		
+	}
+	
 	public void setPoint(Point point) {
 		this.point = point;
 	}
 	protected Point getPosition(){return point;};
-	protected Unit _unit ;
-	public Unit get_unit() {
-		return _unit;
+	protected List<Unit> units ;
+	
+	public List<Unit> getUnits() {
+		return units;
 	}
-	public void set_unit(Unit _unit) {
-		this._unit = _unit;
-		_unit.setPosition(point);
+	public void addUnit(Unit unit) {
+		this.units.add(unit);
+		unit.setPosition(point);
+	}
+	
+	public void removeUnit(Unit unit) {
+		this.units.remove(unit);
+	}
+	
+	public void clearUnits() {
+		this.units.clear();
 	}
 	
 	
